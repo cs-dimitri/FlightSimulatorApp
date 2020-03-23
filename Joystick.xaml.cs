15686b9f01f37230bfa6efa7ab3998ec44a229f2
cs-dimitri <turovetsd@icloud.com>
2020-03-23 13:56:16 +0200

    Add files via upload

--- Joystick.xaml.cs
+++ Joystick.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,25 +16,45 @@ namespace HomeWork
 {
     public partial class Joystick
     {
+        private const double lookBetter = 50;
+
         public double x1 { set; get; }
         public double y1 { set; get; }
 
         private double initX;
         private double initY;
 
+
         public bool mouseIsPressed = false;
         public bool mouseReturn = false;
 
+        private double radiusKnobBase;
+
+     
+
+        private double radius_Small;
+        private double radius_Outside;
+        double calculatedElevator;
+        double calculatedRudder;
+        private Point position_TO_MOVE;
+        private bool checkBoard;
+        
 
         public Joystick()
         {
            InitializeComponent();
             initX = this.knobPosition.X;  
             initY = this.knobPosition.Y;
-            
+
+            radiusKnobBase = KnobBase.Height / 2;
+
+            radius_Small = KnobBase.Height / 2;
+            radius_Outside = OuttestEllipse.Width / 2;
+
+            dummy_centre.Fill = new SolidColorBrush(Colors.Red);
 
 
-
+    
         }
 
         private void MouseLeftDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -49,12 +70,15 @@ namespace HomeWork
         }
         private void MouseMove_J(object sender, System.Windows.Input.MouseEventArgs e)
         {
+
+
+
+
             Point position = e.GetPosition(dummy_centre);
 
             x1 = position.X;
             y1 = position.Y;
-            double radius_Small = KnobBase.Height / 2;
-            double radius = OuttestEllipse.Width / 2;
+
 
 
 
@@ -62,18 +86,26 @@ namespace HomeWork
             if (mouseIsPressed)
             {
 
-                bool checkBoard = (Math.Pow(x1 + knobPosition.X  - initX, 2) + 
-                    Math.Pow(y1 + knobPosition.Y - initY, 2)) <= Math.Pow(radius - radius_Small + 50, 2);
+                checkBoard = (Math.Pow(x1 + knobPosition.X  - initX, 2) + 
+                    Math.Pow(y1 + knobPosition.Y - initY, 2)) <= Math.Pow(radius_Outside - radius_Small + lookBetter, 2);
                 if (checkBoard)
                 {
                     knobPosition.X += x1;
                     knobPosition.Y += y1;
-   
+
+                    
+                    
+                    position_TO_MOVE = getTruePoint(e.GetPosition(OuttestEllipse));
+                    
+                    calculatedRudder = (position_TO_MOVE.X) / (radius_Outside + lookBetter - radius_Small);
+                    calculatedElevator = (position_TO_MOVE.Y) / (radius_Outside + lookBetter - radius_Small);
+
+
                 }
                 else
                 {
                     mouseReturn = true;
-                   //ellipse_part_3.Height = 900;
+                   
                 }
             }
         }
@@ -86,15 +118,46 @@ namespace HomeWork
         private void MouseReturnToJOY(object sender, MouseEventArgs e)
         {
             Point position = e.GetPosition(dummy_centre);
-             
-            //Point position = e.GetPosition(Base);
+
+
             if (mouseIsPressed)
             {
                 knobPosition.X -= -position.X ;
                 knobPosition.Y -= -position.Y ;
             }
         }
+
+        private Point getTruePoint(Point pointToCorrect)
+        {
+            double xToSet = 0, yToSet = 0;
+            double xPoint = pointToCorrect.X;
+            double yPoint = pointToCorrect.Y;
+
+            if (xPoint <= radius_Outside && yPoint <= radius_Outside) // first 
+            {
+                xToSet = -(radius_Outside - xPoint);
+                yToSet = radius_Outside - yPoint;
+            }else if (xPoint > radius_Outside && yPoint < radius_Outside) // second
+            {
+                xToSet = xPoint - radius_Outside;
+                yToSet = radius_Outside - yPoint;
+            }else if (xPoint > radius_Outside && yPoint > radius_Outside) // 4
+            {
+                xToSet = xPoint - radius_Outside;
+                yToSet = -(yPoint - radius_Outside);
+            }else if (xPoint < radius_Outside && yPoint > radius_Outside) //3
+            {
+
+                xToSet = -(radius_Outside - xPoint);
+                yToSet = -(yPoint - radius_Outside);
+            }
+
+
+            return new Point(xToSet,yToSet);
+        }
     }
+
+  
 
    
 
